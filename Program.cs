de06c0fa8dc47c88fd37e9898c2395fa440e5281
Jackson Lin <jackson1994y@gmail.com>
2020-09-17 20:18:04 -0400

    init commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace InheritanceOverride
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CityDerivedClass myDerivedCity = new CityDerivedClass("New York");
+            myDerivedCity.printCityName();
+        }
+    }
+
+    public abstract class CityBaseClass{
+        public string name;
+        public CityBaseClass(string name){
+            this.name = name;
+        }
+        public virtual void printCityName(){
+            Console.Write("This is a virtual output");
+        }
+    }
+
+    class CityDerivedClass:CityBaseClass{
+        public CityDerivedClass(string name):base(name){
+        }
+        public override void printCityName(){
+            Console.Write("This overrides the virtual method");
+        }
+    }
+}
